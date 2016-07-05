b0e67276e6868cf85dd811e6d686ea16d415b2c4
Cristian Cortez <cortez.cristian@gmail.com>
2016-07-05 10:45:50 -0300

    Update package version

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.4.1');
+  api.versionsFrom('1.3.3');
   api.use('ecmascript');
   api.mainModule('meteor-plivo.js', 'server');
 });
